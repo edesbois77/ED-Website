b329c9ea0c5f6071a6123d303900fa6e71cf4a7f
edesbois77 <edesbois@yahoo.co.uk>
2025-07-04 09:19:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ED-Website/', // 👈 Add this line!
+  base: '/ED-Website/',
   plugins: [react()],
 });
