61c1a924a579cb3459b9d92147f06b8d67be04ec
Edward Desbois <your@email.comedesbois@yahoo.co.uk>
2025-07-04 09:16:20 +0100

    Add your commit message here

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ED-Website/', // 👈 Add this line!
   plugins: [react()],
 });
