0bd4dc0c9d28f14748e3655964c7a434f931380b
edesbois77 <edesbois@yahoo.co.uk>
2025-07-08 13:10:11 +0100

    Updated PromotionalBar.tsx

--- src/components/PromotionalBar.tsx
+++ src/components/PromotionalBar.tsx
@@ -18,7 +18,7 @@ const PromotionalBar: React.FC = () => {
   };
 
   return (
-    <div className="bg-[#b4b4b4] border-b border-gray-200 overflow-hidden">
+    <div className="bg-[#0dea2c] border-b border-gray-200 overflow-hidden">
       <div className="relative">
         {/* Scrolling content */}
         <div className="animate-scroll flex whitespace-nowrap py-3">
