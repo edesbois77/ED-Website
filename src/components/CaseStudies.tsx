ff69a566aa4e407a210179ec6465fb9e8f5fa2bc
edesbois77 <edesbois@yahoo.co.uk>
2025-07-08 15:42:01 +0100

    Updated CaseStudies.tsx

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -18,7 +18,7 @@ const CaseStudies: React.FC = () => {
             Success Stories
           </h2>
           <p className="text-base sm:text-lg text-gray-600 max-w-2xl mx-auto font-light">
-            Discover how leading companies transformed their operations by utilising ED.
+            Discover how top brands exceeded their goals with ED products.
           </p>
         </div>
 
