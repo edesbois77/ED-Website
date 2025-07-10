af28c66d10459e90d544c099f2bedfda9e07b1d3
edesbois77 <edesbois@yahoo.co.uk>
2025-07-10 13:43:47 +0100

    Fix mobile deployment asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ED-Website/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'docs',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 });
