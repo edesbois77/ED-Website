fb600c225e2eddffe5f2362f4f0988967f36b15a
edesbois77 <edesbois@yahoo.co.uk>
2025-07-17 18:01:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ED-Website/',    // for GitHub Pages project site
+  base: '/',    // for GitHub Pages project site
   plugins: [react()],
   build: {
     outDir: 'docs',        // output to /docs for Pages
