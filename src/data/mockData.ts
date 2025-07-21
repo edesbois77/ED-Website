1a8c603ab5e5bef533c5deef54ed672e506308c0
edesbois77 <edesbois@yahoo.co.uk>
2025-07-21 17:29:02 +0100

    Updated mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -200,7 +200,7 @@ export const caseStudies: CaseStudy[] = [
   {
     id: '3',
     title: 'Next In Line: The Future of Football with FedEx',
-    description: 'This is dummy data... the full case study is yet to be added',
+    description: 'Positioned FedEx as an innovator in European football by connecting Champions League fans with emerging stars through talent-led, multi-platform content and data-driven engagement.',
     image: 'https://i.imgur.com/ubcUIH7.jpeg?auto=compress&cs=tinysrgb&w=800',
     link: '#'
   }
