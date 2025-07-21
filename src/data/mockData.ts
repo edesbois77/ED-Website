ab6c9fe15ded892b5836380a219c839a08ca52bb
edesbois77 <edesbois@yahoo.co.uk>
2025-07-21 17:24:03 +0100

    Updated mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -201,7 +201,7 @@ export const caseStudies: CaseStudy[] = [
     id: '3',
     title: 'Next In Line: The Future of Football with FedEx',
     description: 'This is dummy data... the full case study is yet to be added',
-    image: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://i.imgur.com/ubcUIH7.jpeg?auto=compress&cs=tinysrgb&w=800',
     link: '#'
   }
 ];
