2e77294f2b3891ed160a5bf3041918e647fd2f95
edesbois77 <edesbois@yahoo.co.uk>
2025-07-21 16:47:02 +0100

    Updated caseStudiesData.ts

--- src/data/caseStudiesData.ts
+++ src/data/caseStudiesData.ts
@@ -140,7 +140,7 @@ By putting Coca-Cola Zero Sugar at the center of the Euro2024 fan experience, we
         'https://i.imgur.com/0dHALua.jpeg?auto=compress&cs=tinysrgb&w=800',
         'https://i.imgur.com/prWkwB6.jpeg?auto=compress&cs=tinysrgb&w=800',
         'https://i.imgur.com/24NwBv5.jpeg?auto=compress&cs=tinysrgb&w=800',
-        'https://i.imgur.com/pbX2QQX.gif?auto=compress&cs=tinysrgb&w=800'
+        'https://i.imgur.com/I29rSV7.gif?auto=compress&cs=tinysrgb&w=800'
       ]
     },
     tags: ['UEFA Championa League', 'Community Building', 'Content Strategy', 'Analytics'],
